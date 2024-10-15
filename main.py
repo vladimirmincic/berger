fa4eda31211d6468c45cc932fafaba293eda9bb9
Vladimir Minčić <vlada@mincic.in.rs>
2024-10-15 03:08:02 +0200

    Update

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
-import os
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.application import MIMEApplication
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
-from dotenv import load_dotenv
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from rounds import get_rounds_data
+from datetime import datetime
 
-# Load environment variables
-load_dotenv()
+rounds = 14
+tournament_name = "Tournament"
 
-class BergerSchedule:
-    def __init__(self, tournament_name):
-        self.tournament = tournament_name
 
-    def create_pdf(self, filename, teams, rounds):
-        custom_page_size = (2200, 500)
-        doc = SimpleDocTemplate(filename, pagesize=custom_page_size)
+class BergerTournamentPDF:
+    def __init__(self, rounds, tournament):
+        self.rounds = rounds
+        self.tournament = tournament
+        self.filename = self.generate_filename()
 
-        # Create table data
-        table_data = [["Berger Schedule", self.tournament]] + rounds
+    def generate_filename(self):
+        # Format the current date
+        current_date = datetime.now().strftime("%d-%m-%Y")
+        # Generate the PDF filename
+        return f"berger_{self.rounds}_{self.tournament}_{current_date}.pdf"
 
-        # Create table
+    def create_pdf(self):
+        custom_page_size = (1500, 500)
+
+        # Create a PDF document with custom page size
+        doc = SimpleDocTemplate(self.filename, pagesize=custom_page_size)
+
+        # Get table data from rounds.py
+        table_data = get_rounds_data(self.rounds)
+
+        # Create styles for the paragraph
+        styles = getSampleStyleSheet()
+        title_style = styles['Title']
+
+        # Create the tournament title as a Paragraph
+        tournament_title = Paragraph(self.tournament, title_style)
+
+        # Create the table
         table = Table(table_data)
 
         # Apply styles to the table
@@ -31,52 +46,14 @@ class BergerSchedule:
                             ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
                             ('BACKGROUND', (0, 0), (-1, 0), (0.9, 0.9, 0.9)),
                             ('GRID', (0, 0), (-1, -1), 1, (0, 0, 0))])
+
         table.setStyle(style)
 
-        doc.build([table])
+        # Build the PDF document with the title and table
+        elements = [tournament_title, table]
+        doc.build(elements)
 
-class EmailSender:
-    def __init__(self):
-        self.smtp_server = os.getenv("SMTP_SERVER")
-        self.smtp_port = os.getenv("SMTP_PORT")
-        self.smtp_username = os.getenv("SMTP_USERNAME")
-        self.smtp_password = os.getenv("SMTP_PASSWORD")
-        self.sender_email = os.getenv("SENDER_EMAIL")
-        self.receiver_email = os.getenv("RECEIVER_EMAIL")
-
-    def send_email(self, subject, body, attachment_filename):
-        try:
-            message = MIMEMultipart()
-            message["From"] = self.sender_email
-            message["To"] = self.receiver_email
-            message["Subject"] = subject
-            message.attach(MIMEText(body, "plain"))
-
-            with open(attachment_filename, "rb") as attachment:
-                part = MIMEApplication(attachment.read(), Name=attachment_filename)
-                part['Content-Disposition'] = f'attachment; filename="{attachment_filename}"'
-                message.attach(part)
-
-            server = smtplib.SMTP(self.smtp_server, self.smtp_port)
-            server.starttls()
-            server.login(self.smtp_username, self.smtp_password)
-            server.sendmail(self.sender_email, self.receiver_email, message.as_string())
-            server.quit()
-
-            print("Email sent successfully!")
-        except Exception as e:
-            print(f"Error: {str(e)}")
 
 if __name__ == "__main__":
-    schedule = BergerSchedule("TOURNAMENT - LIGA")
-    teams = [f"ekipa_{i}" for i in range(1, 31)]
-    rounds = [
-        ["Kolo 1", f"{teams[0]}-{teams[3]}", f"{teams[1]}-{teams[2]}"],
-        # Add more rounds here...
-    ]
-
-    pdf_filename = "berger_schedule.pdf"
-    schedule.create_pdf(pdf_filename, teams, rounds)
-
-    email_sender = EmailSender()
-    email_sender.send_email("Berger Schedule", "U prilogu je Berger Schedule", pdf_filename)
+    tournament_pdf = BergerTournamentPDF(rounds, tournament_name)
+    tournament_pdf.create_pdf()
---
+++ rounds.py
@@ -0,0 +1,91 @@
+column = ""
+
+ekipa_1 = "aaaaaaaaaa"
+ekipa_2 = "bbbbbbbbbb"
+ekipa_3 = "cccccccccc"
+ekipa_4 = "dddddddddd"
+ekipa_5 = "eeeeeeeeee"
+ekipa_6 = "ffffffffff"
+ekipa_7 = "gggggggggg"
+ekipa_8 = "hhhhhhhhhh"
+ekipa_9 = "iiiiiiiiii"
+ekipa_10 = "jjjjjjjjjj"
+ekipa_11 = "kkkkkkkkkk"
+ekipa_12 = "llllllllll"
+ekipa_13 = "mmmmmmmmmm"
+ekipa_14 = "nnnnnnnnnn"
+
+def get_rounds_data(rounds):
+    if rounds == 4:
+        return [
+            [column],
+            ["Kolo 1", f"{ekipa_1}-{ekipa_4}", f"{ekipa_2}-{ekipa_3}"],
+            ["Kolo 2", f"{ekipa_4}-{ekipa_3}", f"{ekipa_1}-{ekipa_2}"],
+            ["Kolo 3", f"{ekipa_2}-{ekipa_4}", f"{ekipa_3}-{ekipa_1}"]
+        ]
+    if rounds == 6:
+        return [
+            [column],
+            ["Kolo 1", f"{ekipa_1}-{ekipa_6}", f"{ekipa_2}-{ekipa_5}", f"{ekipa_3}-{ekipa_4}"],
+            ["Kolo 2", f"{ekipa_6}-{ekipa_4}", f"{ekipa_5}-{ekipa_3}", f"{ekipa_1}-{ekipa_2}"],
+            ["Kolo 3", f"{ekipa_2}-{ekipa_6}", f"{ekipa_3}-{ekipa_1}", f"{ekipa_4}-{ekipa_5}"],
+            ["Kolo 4", f"{ekipa_6}-{ekipa_5}", f"{ekipa_1}-{ekipa_4}", f"{ekipa_2}-{ekipa_3}"],
+            ["Kolo 5", f"{ekipa_3}-{ekipa_6}", f"{ekipa_4}-{ekipa_2}", f"{ekipa_5}-{ekipa_1}"],
+        ]
+    if rounds == 8:
+        return [
+            [column],
+            ["Kolo 1", f"{ekipa_1}-{ekipa_8}", f"{ekipa_2}-{ekipa_7}", f"{ekipa_3}-{ekipa_6}", f"{ekipa_4}-{ekipa_5}"],
+            ["Kolo 2", f"{ekipa_8}-{ekipa_5}", f"{ekipa_6}-{ekipa_4}", f"{ekipa_7}-{ekipa_3}", f"{ekipa_1}-{ekipa_2}"],
+            ["Kolo 3", f"{ekipa_2}-{ekipa_8}", f"{ekipa_3}-{ekipa_1}", f"{ekipa_4}-{ekipa_7}", f"{ekipa_5}-{ekipa_6}"],
+            ["Kolo 4", f"{ekipa_8}-{ekipa_6}", f"{ekipa_7}-{ekipa_5}", f"{ekipa_1}-{ekipa_4}", f"{ekipa_2}-{ekipa_3}"],
+            ["Kolo 5", f"{ekipa_3}-{ekipa_8}", f"{ekipa_4}-{ekipa_2}", f"{ekipa_5}-{ekipa_1}", f"{ekipa_6}-{ekipa_7}"],
+            ["Kolo 6", f"{ekipa_8}-{ekipa_7}", f"{ekipa_1}-{ekipa_6}", f"{ekipa_2}-{ekipa_5}", f"{ekipa_3}-{ekipa_4}"],
+            ["Kolo 7", f"{ekipa_4}-{ekipa_8}", f"{ekipa_5}-{ekipa_3}", f"{ekipa_6}-{ekipa_2}", f"{ekipa_7}-{ekipa_1}"],
+        ]
+    if rounds == 10:
+        return [
+            [column],
+            ["Kolo 1", f"{ekipa_1}-{ekipa_10}", f"{ekipa_2}-{ekipa_9}", f"{ekipa_3}-{ekipa_8}", f"{ekipa_4}-{ekipa_7}", f"{ekipa_5}-{ekipa_6}"],
+            ["Kolo 2", f"{ekipa_10}-{ekipa_6}", f"{ekipa_7}-{ekipa_5}", f"{ekipa_8}-{ekipa_4}", f"{ekipa_9}-{ekipa_3}", f"{ekipa_1}-{ekipa_2}"],
+            ["Kolo 3", f"{ekipa_2}-{ekipa_10}", f"{ekipa_3}-{ekipa_1}", f"{ekipa_4}-{ekipa_9}", f"{ekipa_5}-{ekipa_8}", f"{ekipa_6}-{ekipa_7}"],
+            ["Kolo 4", f"{ekipa_10}-{ekipa_7}", f"{ekipa_8}-{ekipa_6}", f"{ekipa_9}-{ekipa_5}", f"{ekipa_1}-{ekipa_4}", f"{ekipa_2}-{ekipa_3}"],
+            ["Kolo 5", f"{ekipa_3}-{ekipa_10}", f"{ekipa_4}-{ekipa_2}", f"{ekipa_5}-{ekipa_1}", f"{ekipa_6}-{ekipa_9}", f"{ekipa_7}-{ekipa_8}"],
+            ["Kolo 6", f"{ekipa_10}-{ekipa_8}", f"{ekipa_9}-{ekipa_7}", f"{ekipa_1}-{ekipa_6}", f"{ekipa_2}-{ekipa_5}", f"{ekipa_3}-{ekipa_4}"],
+            ["Kolo 7", f"{ekipa_4}-{ekipa_10}", f"{ekipa_5}-{ekipa_3}", f"{ekipa_6}-{ekipa_2}", f"{ekipa_7}-{ekipa_1}", f"{ekipa_8}-{ekipa_9}"],
+            ["Kolo 8", f"{ekipa_10}-{ekipa_9}", f"{ekipa_1}-{ekipa_8}", f"{ekipa_2}-{ekipa_7}", f"{ekipa_3}-{ekipa_6}", f"{ekipa_4}-{ekipa_5}"],
+            ["Kolo 9", f"{ekipa_5}-{ekipa_10}", f"{ekipa_6}-{ekipa_4}", f"{ekipa_7}-{ekipa_3}", f"{ekipa_8}-{ekipa_2}", f"{ekipa_9}-{ekipa_1}"],
+        ]
+    if rounds == 12:
+        return [
+            [column],
+            ["Kolo 1", f"{ekipa_1}-{ekipa_12}", f"{ekipa_2}-{ekipa_11}", f"{ekipa_3}-{ekipa_10}", f"{ekipa_4}-{ekipa_9}", f"{ekipa_5}-{ekipa_8}", f"{ekipa_6}-{ekipa_7}"],
+            ["Kolo 2", f"{ekipa_12}-{ekipa_7}", f"{ekipa_8}-{ekipa_6}", f"{ekipa_9}-{ekipa_5}", f"{ekipa_10}-{ekipa_4}", f"{ekipa_11}-{ekipa_3}", f"{ekipa_1}-{ekipa_2}"],
+            ["Kolo 3", f"{ekipa_2}-{ekipa_12}", f"{ekipa_3}-{ekipa_1}", f"{ekipa_4}-{ekipa_11}", f"{ekipa_5}-{ekipa_10}", f"{ekipa_6}-{ekipa_9}", f"{ekipa_7}-{ekipa_8}"],
+            ["Kolo 4", f"{ekipa_12}-{ekipa_8}", f"{ekipa_9}-{ekipa_7}", f"{ekipa_10}-{ekipa_6}", f"{ekipa_11}-{ekipa_5}", f"{ekipa_1}-{ekipa_4}", f"{ekipa_2}-{ekipa_3}"],
+            ["Kolo 5", f"{ekipa_3}-{ekipa_12}", f"{ekipa_4}-{ekipa_2}", f"{ekipa_5}-{ekipa_1}", f"{ekipa_6}-{ekipa_11}", f"{ekipa_7}-{ekipa_10}", f"{ekipa_8}-{ekipa_9}"],
+            ["Kolo 6", f"{ekipa_12}-{ekipa_9}", f"{ekipa_10}-{ekipa_8}", f"{ekipa_11}-{ekipa_7}", f"{ekipa_1}-{ekipa_6}", f"{ekipa_2}-{ekipa_5}", f"{ekipa_3}-{ekipa_4}"],
+            ["Kolo 7", f"{ekipa_4}-{ekipa_12}", f"{ekipa_5}-{ekipa_3}", f"{ekipa_6}-{ekipa_2}", f"{ekipa_7}-{ekipa_1}", f"{ekipa_8}-{ekipa_11}", f"{ekipa_9}-{ekipa_10}"],
+            ["Kolo 8", f"{ekipa_12}-{ekipa_10}", f"{ekipa_11}-{ekipa_9}", f"{ekipa_1}-{ekipa_8}", f"{ekipa_2}-{ekipa_7}", f"{ekipa_3}-{ekipa_6}", f"{ekipa_4}-{ekipa_5}"],
+            ["Kolo 9", f"{ekipa_5}-{ekipa_12}", f"{ekipa_6}-{ekipa_4}", f"{ekipa_7}-{ekipa_3}", f"{ekipa_8}-{ekipa_2}", f"{ekipa_9}-{ekipa_1}", f"{ekipa_10}-{ekipa_11}"],
+            ["Kolo 10", f"{ekipa_12}-{ekipa_11}", f"{ekipa_1}-{ekipa_10}", f"{ekipa_2}-{ekipa_9}", f"{ekipa_3}-{ekipa_8}", f"{ekipa_4}-{ekipa_7}", f"{ekipa_5}-{ekipa_6}"],
+            ["Kolo 11", f"{ekipa_6}-{ekipa_12}", f"{ekipa_7}-{ekipa_5}", f"{ekipa_8}-{ekipa_4}", f"{ekipa_9}-{ekipa_3}", f"{ekipa_10}-{ekipa_2}", f"{ekipa_11}-{ekipa_1}"],
+        ]
+    if rounds == 14:
+        return [
+            [column],
+            ["Kolo 1", f"{ekipa_1}-{ekipa_14}", f"{ekipa_2}-{ekipa_13}", f"{ekipa_3}-{ekipa_12}", f"{ekipa_4}-{ekipa_11}", f"{ekipa_5}-{ekipa_10}", f"{ekipa_6}-{ekipa_9}", f"{ekipa_7}-{ekipa_8}"],
+            ["Kolo 2", f"{ekipa_14}-{ekipa_8}", f"{ekipa_9}-{ekipa_7}", f"{ekipa_10}-{ekipa_6}", f"{ekipa_11}-{ekipa_5}", f"{ekipa_12}-{ekipa_4}", f"{ekipa_13}-{ekipa_3}", f"{ekipa_1}-{ekipa_2}"],
+            ["Kolo 3", f"{ekipa_2}-{ekipa_14}", f"{ekipa_3}-{ekipa_1}", f"{ekipa_4}-{ekipa_13}", f"{ekipa_5}-{ekipa_12}", f"{ekipa_6}-{ekipa_11}", f"{ekipa_7}-{ekipa_10}", f"{ekipa_8}-{ekipa_9}"],
+            ["Kolo 4", f"{ekipa_14}-{ekipa_9}", f"{ekipa_10}-{ekipa_8}", f"{ekipa_11}-{ekipa_7}", f"{ekipa_12}-{ekipa_6}", f"{ekipa_13}-{ekipa_5}", f"{ekipa_1}-{ekipa_4}", f"{ekipa_2}-{ekipa_3}"],
+            ["Kolo 5", f"{ekipa_3}-{ekipa_14}", f"{ekipa_4}-{ekipa_2}", f"{ekipa_5}-{ekipa_1}", f"{ekipa_6}-{ekipa_13}", f"{ekipa_7}-{ekipa_12}", f"{ekipa_8}-{ekipa_11}", f"{ekipa_9}-{ekipa_10}"],
+            ["Kolo 6", f"{ekipa_14}-{ekipa_10}", f"{ekipa_11}-{ekipa_9}", f"{ekipa_12}-{ekipa_8}", f"{ekipa_13}-{ekipa_7}", f"{ekipa_1}-{ekipa_6}", f"{ekipa_2}-{ekipa_5}", f"{ekipa_3}-{ekipa_4}"],
+            ["Kolo 7", f"{ekipa_4}-{ekipa_14}", f"{ekipa_5}-{ekipa_3}", f"{ekipa_6}-{ekipa_2}", f"{ekipa_7}-{ekipa_1}", f"{ekipa_8}-{ekipa_13}", f"{ekipa_9}-{ekipa_12}", f"{ekipa_10}-{ekipa_11}"],
+            ["Kolo 8", f"{ekipa_14}-{ekipa_11}", f"{ekipa_12}-{ekipa_10}", f"{ekipa_13}-{ekipa_9}", f"{ekipa_1}-{ekipa_8}", f"{ekipa_2}-{ekipa_7}", f"{ekipa_3}-{ekipa_6}", f"{ekipa_4}-{ekipa_5}"],
+            ["Kolo 9", f"{ekipa_5}-{ekipa_14}", f"{ekipa_6}-{ekipa_4}", f"{ekipa_7}-{ekipa_3}", f"{ekipa_8}-{ekipa_2}", f"{ekipa_9}-{ekipa_1}", f"{ekipa_10}-{ekipa_13}", f"{ekipa_11}-{ekipa_12}"],
+            ["Kolo 10", f"{ekipa_14}-{ekipa_12}", f"{ekipa_13}-{ekipa_11}", f"{ekipa_1}-{ekipa_10}", f"{ekipa_2}-{ekipa_9}", f"{ekipa_3}-{ekipa_8}", f"{ekipa_4}-{ekipa_7}", f"{ekipa_5}-{ekipa_6}"],
+            ["Kolo 11", f"{ekipa_6}-{ekipa_14}", f"{ekipa_7}-{ekipa_5}", f"{ekipa_8}-{ekipa_4}", f"{ekipa_9}-{ekipa_3}", f"{ekipa_10}-{ekipa_2}", f"{ekipa_11}-{ekipa_1}", f"{ekipa_12}-{ekipa_13}"],
+            ["Kolo 12", f"{ekipa_14}-{ekipa_13}", f"{ekipa_1}-{ekipa_12}", f"{ekipa_2}-{ekipa_11}", f"{ekipa_3}-{ekipa_10}", f"{ekipa_4}-{ekipa_9}", f"{ekipa_5}-{ekipa_8}", f"{ekipa_6}-{ekipa_7}"],
+            ["Kolo 13", f"{ekipa_7}-{ekipa_14}", f"{ekipa_8}-{ekipa_6}", f"{ekipa_9}-{ekipa_5}", f"{ekipa_10}-{ekipa_4}", f"{ekipa_11}-{ekipa_3}", f"{ekipa_12}-{ekipa_2}", f"{ekipa_13}-{ekipa_1}"],
+        ]
+    return []
